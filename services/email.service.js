4d5fa77a72d8ba8772e9324ae5451e47d89fa557
KARTIK-44 <kartikpatilcs@gmail.com>
2026-06-17 19:21:40 +0530

    fix: dynamically resolve smtp.gmail.com to IPv4 for each email dispatch

--- services/email.service.js
+++ services/email.service.js
@@ -2,18 +2,29 @@ const nodemailer = require("nodemailer");
 const dns = require("dns");
 
 const mailer = (userId, password, mailData) => {
-  return nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    lookup: (hostname, options, callback) => {
-      dns.lookup(hostname, { family: 4 }, callback);
-    },
-    auth: {
-      user: userId,
-      pass: password,
-    },
-  });
+  return {
+    sendMail: (mailData, callback) => {
+      dns.lookup('smtp.gmail.com', { family: 4 }, (err, address) => {
+        if (err) {
+          console.error("[MAILER_DNS] DNS lookup failed for smtp.gmail.com:", err.message);
+          return callback(err);
+        }
+        const transporter = nodemailer.createTransport({
+          host: address,
+          port: 465,
+          secure: true,
+          tls: {
+            servername: 'smtp.gmail.com'
+          },
+          auth: {
+            user: userId,
+            pass: password,
+          },
+        });
+        transporter.sendMail(mailData, callback);
+      });
+    }
+  };
 };
 
 module.exports = mailer;
