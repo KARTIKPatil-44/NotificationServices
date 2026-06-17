937edbca16d318811473910024c389bf26d52bfa
KARTIK-44 <kartikpatilcs@gmail.com>
2026-06-17 18:54:26 +0530

    fix: force IPv4 DNS lookup inside nodemailer SMTP transporter

--- services/email.service.js
+++ services/email.service.js
@@ -1,8 +1,12 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
 
 const mailer = (userId, password, mailData) => {
   return nodemailer.createTransport({
     service: "Gmail",
+    lookup: (hostname, options, callback) => {
+      dns.lookup(hostname, { family: 4 }, callback);
+    },
     auth: {
       user: userId,
       pass: password,
